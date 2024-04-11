9ce95cb25f5a05cce2d59ad134a949a9a4400216
Juanes <trianajuan95@gmail.com>
2024-04-10 20:36:03 -0800

    Update005

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+//Class Activity 1
+
 alert("Hello world");
+
+//Class Activity 2
+
+let NAME = "Juanes";
+let AGE = 19;
+let MALE = true;
+let GIRLFRIEND = undefined;
+let BESTFRIEND = null;
+const HEIGHT = 1.70 ;
+
+console.log ("My name is:" + NAME);
+console.log ("I'm "+ AGE + "years old");
+console.log ("He's a male? :"+ MALE);
+console.log ("My girlfriend is:"+ GIRLFRIEND);
+console.log ("My bestfirend is:"+ BESTFRIEND);
+console.log ("My height is:"+ HEIGHT);
+
+//Class Activity 3
+
+
